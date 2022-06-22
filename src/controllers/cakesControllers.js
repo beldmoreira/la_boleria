10bb65fda3435128e36f65f9451574b2b78a41da
Isabela <isabela_cmoreira@hotmail.com>
2022-06-22 16:29:39 -0300

    feat: repository for the cake route

---
+++ src/controllers/cakesControllers.js
@@ -0,0 +1,30 @@
+import nodemon from "nodemon"
+import * as cakesRepository from "../repositories/cakesRepository.js"
+
+const createCake = async (req,res) => {
+    const {name, price, description, image} = req.body
+    try {
+        console.log (price)
+        if (name.length < 2 || price <= 0){
+            return res.sendStatus(400);
+        }
+        const cake = await cakesRepository.getCakeByName(name)
+        
+        if (cake.rows[0]) {
+            return res.status(409).json({message: "There is already a cake with this name. Register it with another one" });
+        }
+        const result = await cakesRepository.postCakes(name,price,description,image)
+        console.log(result)
+        res.status(201).send(result);
+    } catch (e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
+const cakesControllers = {
+    createCake
+}
+
+export default cakesControllers;
+
