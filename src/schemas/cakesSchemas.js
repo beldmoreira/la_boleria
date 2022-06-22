428fde004afcd59b51105af3ba0dfa08ededd1ef
Isabela <isabela_cmoreira@hotmail.com>
2022-06-22 00:39:06 -0300

    feat: added a minor requirement in the joi validation

--- src/schemas/cakesSchemas.js
+++ src/schemas/cakesSchemas.js
@@ -1,9 +1,10 @@
 import Joi from "joi";
 
-export const PostCakes = Joi.object({
+export const cakesSchemas = Joi.object({
     name: Joi
     .string()
     .pattern(/^\w{2,}$/)
+    .min(2)
     .required(),
     price:Joi
     .number()
@@ -16,7 +17,6 @@ export const PostCakes = Joi.object({
     .uri()
     .required()
 });
-
 
 
 
