da26be7852dccbbfa789dedbcdfbfc939bf5a3ba
Isabela <isabela_cmoreira@hotmail.com>
2022-06-23 20:47:26 -0300

    feat: added all the routers

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,13 @@
 import {Router} from "express";
 import cakesRouter from "../routers/cakesRouter.js";
+import ordersRouter from "../routers/ordersRouter.js"
+import clientsRouter from "./clientsRouter.js";
 
 const router = Router();
 
 router.use(cakesRouter)
+router.use(clientsRouter)
+router.use(ordersRouter)
 
 
 export default router;
