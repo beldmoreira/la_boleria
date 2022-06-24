20394044c385e8748e684a680390202e3b63c2b9
Isabela <isabela_cmoreira@hotmail.com>
2022-06-24 12:35:24 -0300

    feat: added cakes controller for its routes

--- src/controllers/cakesControllers.js
+++ src/controllers/cakesControllers.js
@@ -1,10 +1,9 @@
-import nodemon from "nodemon"
 import * as cakesRepository from "../repositories/cakesRepository.js"
 
 const createCake = async (req,res) => {
     const {name, price, description, image} = req.body
     try {
-        console.log (price)
+        
         if (name.length < 2 || price <= 0){
             return res.sendStatus(400);
         }
@@ -14,7 +13,7 @@ const createCake = async (req,res) => {
             return res.status(409).json({message: "There is already a cake with this name. Register it with another one" });
         }
         const result = await cakesRepository.postCakes(name,price,description,image)
-        console.log(result)
+       
         res.status(201).send(result);
     } catch (e){
         console.log(e);
